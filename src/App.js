a0422279d1753485e8d51a351f5b6c16edb2215b
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2024-10-30 19:34:14 +0530

    filter-implementation

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import TodoSearch from "./components/TodoSearch";
+//context
+import { useTodoContext } from "./context/TodoContext";
 
 
 
 function App() {
+  const { state,dispatch} = useTodoContext();
+
+  const handleFilterChange = (filter) => {
+    dispatch({ type: 'SET_FILTER', filter });
+  };
+
+
   return (
     <div className="App">
       <div className="top-header">
         <h1>Today</h1>
         <TodoSearch />
         <div className="todo-filter-container">
-          <div className="todo-filter">
-            All
+        {['all', 'completed', 'incomplete'].map(filter => (
+          <div
+            key={filter}
+            style={{
+              backgroundColor: state.filter === filter ? 'green' : '#f0f0f0',
+              color: state.filter === filter ? '#fff' : '#000',
+            }}
+            onClick={() => handleFilterChange(filter)}
+            className="todo-filter"
+          >
+            {filter.charAt(0).toUpperCase() + filter.slice(1)}
           </div>
-          <div className="todo-filter">
-            Completed
-          </div>
-          <div className="todo-filter">
-            Incomplete
-          </div>
+        ))}
         </div>
       </div>
       <TodoList />
